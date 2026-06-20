4c2bed799f4b2412e81d8920b69cea442d7e314e
Mikhail <govgolenkom@icloud.com>
2026-06-20 14:47:36 +0300

    Add GitHub Pages deployment workflow

--- app-tauri/vite.config.ts
+++ app-tauri/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
+const base = process.env.TAURI_ENV_PLATFORM ? "/" : "/PDF-Converter/";
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
+  base,
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
